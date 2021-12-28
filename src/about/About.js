4c483c20e4173062eabba27e9a8356fac954f7f7
Dafna <margalit.dafna77@gmail.com>
2021-12-27 17:39:55 -0700

    about page done

--- src/about/About.js
+++ src/about/About.js
@@ -2,37 +2,46 @@ import React from 'react';
 import Navbar from '../navbar/Navbar';
 import styled from 'styled-components';
 import AboutPhoto from '../assets/images/dafna-prof.png';
-import AboutPhoto2 from '../assets/images/always-a-dream-film.png';
+// import AboutPhoto2 from '../assets/images/always-a-dream-film.png';
+import AboutPhoto3 from '../assets/images/dafna-photo.jpg';
+
 const AboutWrap = styled.div`
     height: 100vh;
+    overflow: scroll;
+    width: 100vw;
 
 `
-
 const NavbarHeader = styled.div`
     margin-top: 7vh;
     height: 8vh;
 `
 
-const ImageWrap = styled.div`
-    height: 100vh;
-    left: 0;
-    position: fixed;
-`
-const ImageWrap2 = styled.div`
-    left: 50;
-    bottom: 0;
-    
-    position: fixed;
-`
+// const ImageWrap = styled.div`
+//     height: 90vh;
+//     left: 0;
+//     position: fixed;
+//     // top: 100;
+//     overflow: scroll;
+// `
+
 const Body = styled.div`
-    display: flex;
-    flex-direction: row;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content:center;
+        overflow: scroll;
+`
+
+const DescriptionWrap = styled.div`
+    color: #17246c;
+    background-color: #b6c8e3;
+    width: 100%;
 `
 
 const Description = styled.div`
-    color: white;
+    text-align: center;
+    padding: 2em;
 `
-
 const About = () => {
     return(
         <AboutWrap>
@@ -40,15 +49,16 @@ const About = () => {
             <Navbar/>
         </NavbarHeader>
         <Body>
-        <ImageWrap>
-            <img width="600px" src={AboutPhoto} alt=""></img>
-        </ImageWrap>
-        <ImageWrap2>
-        <img width="600px" src={AboutPhoto2} alt=""></img>
-        </ImageWrap2>
-        <Description>
-            Dafna is 21
-        </Description>
+            <img width="100%" src={AboutPhoto} alt=""></img>
+            <DescriptionWrap>
+                <Description>
+                A 21 year old singer, songwriter and producer from Boulder, Dafna eclectic taste brings forth an exciting combination of sounds. Practicing classical piano since the age of 4, Dafna’s interest in music began early on, and she was 11 years old when she began writing and singing her own songs. 
+                It wasn’t until she graduated High School at the age of 17, however, that Dafna began producing and recording her music from her bedroom in her parents basement, which led to the subsequent release of her first single in January 2019, if you’re a dove, a “hypnotic blend of folk and minimalist electronic pop (Amelia Vandergast, A&R Factory). Since then, Dafna has independently released 10 other songs and two albums amassing over one million streams on Spotify alone. 
+                While attending University of Colorado, Boulder as an Electrical and Engineering student, Dafna continues to play local shows with her own band, her “quippy sense of humor and powerful vocals” allowing for an exciting and energetic show (Reilly Ernst, Crave The Sound). Notable performances include headlining a sold out show at the Boulder Theatre, headlining the Fox Theatre, playing the main stage at Westword Music Showcase, and singing with Covex on the main stage at Decadence NYE and Red Rocks. 
+                (Photos taken by Jivan West)
+                </Description>
+            </DescriptionWrap>
+            <img width="100%" src={AboutPhoto3} alt=""></img>
         </Body>
         </AboutWrap>
     )
